aba416b2046e18ec1f74ff62fff1e63e5c55e833
scotttyso <tyscott@cisco.com>
2021-01-14 20:59:56 +0000

    added department

--- main.tf
+++ main.tf
@@ -116,6 +116,7 @@ resource "aws_eip_association" "hashicat" {
 }
 
 resource "aws_instance" "hashicat" {
+  Department                  = devops
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
